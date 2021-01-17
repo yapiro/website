821c8ba36822303a034ffae4f9d2486d0f2d43f4
EC2 Default User <ec2-user@ip-172-31-81-236.ec2.internal>
2021-01-17 05:37:56 +0000

    first commit

---
+++ test-carousel.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+})
+
+$(document).ready(function(){
+  $("#sp-only").click(function(){
+    $(".sponly").slideToggle();
+  });
+});
+
